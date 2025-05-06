8fba95b62dbc4625c151cc6a59b6c75eee973a6d
Shane Yost <scy@shaneyost.io>
2025-05-05 21:14:04 -0600

    Added a demo to test out new mod

--- app.lua
+++ app.lua
@@ -3,9 +3,25 @@ local ffi = require("ffi")
 local gen = require("tlmgen.tlmgen")
 local cfg = require("config")
 
-local Constructor = gen.create_struct(cfg[1])
-local Model = Constructor()
-print(Model)
-print("last_error: ", ffi.string(Model.last_error))
-ffi.copy(Model.last_error, "bad")
-print("last_error: ", ffi.string(Model.last_error))
+packets = {}
+
+-- Create the Ctype constructor and allocate our packets
+for i = 1, #cfg do
+    local Constructor = gen.create_struct(cfg[i])
+    table.insert(packets, Constructor())
+end
+
+-- An example of assinging value to char memeber via ffi
+ffi.copy(packets[1].last_error, "bad")
+ffi.copy(packets[2].operator, "shane")
+ffi.copy(packets[3].comment, "hello")
+
+-- An example of printing char members via ffi
+print("trash_compactor last_error: ", ffi.string(packets[1].last_error))
+print("superlaser_control operator: ", ffi.string(packets[2].operator))
+print("reactor_core comment: ", ffi.string(packets[3].comment))
+
+-- Looping on our new objects
+for i = 1, #cfg do
+    print(packets[i])
+end
